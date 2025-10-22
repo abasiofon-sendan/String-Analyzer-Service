5d39895d5f72e4cd4dd1ac60b2fda4a0cc7f71c9
Abasiofon Sendan <150977482+abasiofon-sendan@users.noreply.github.com>
2025-10-22 23:30:33 +0100

    Worked on the urls

--- Analyzer/urls.py
+++ Analyzer/urls.py
@@ -6,7 +6,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path('strings', StringAnalysisView.as_view(), name='string-list-create'),
-    path('strings/filter-by-natural-language', NaturalLanguageFilterView.as_view(), name='natural-filter'),
-    path('strings/<str:string_value>', SpecificStringview.as_view(), name='string-detail'),
+    path('', StringAnalysisView.as_view(), name='string-list-create'),
+    path('/filter-by-natural-language', NaturalLanguageFilterView.as_view(), name='natural-filter'),
+    path('/<str:string_value>', SpecificStringview.as_view(), name='string-detail'),
 ]
