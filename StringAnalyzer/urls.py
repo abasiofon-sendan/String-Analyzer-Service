7c6193a4d865e5f34e6bc6be9d31467f6ba1e9a4
Abasiofon Sendan <150977482+abasiofon-sendan@users.noreply.github.com>
2025-10-22 23:31:59 +0100

    Update urls.py

--- StringAnalyzer/urls.py
+++ StringAnalyzer/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("Analyzer.urls"))
+    path('strings',include("Analyzer.urls"))
 ]
